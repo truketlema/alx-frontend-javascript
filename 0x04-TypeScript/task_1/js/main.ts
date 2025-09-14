b048693e59358c914e9d4e60229ce001faf4b7a0
Truket Lema <truketlema9489@gmail.com>
2025-09-14 17:06:46 +0300

    Add printTeacher function and interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -32,3 +32,14 @@ const director1: Director = {
 
 console.log(teacher3);
 console.log(director1);
+
+// Task 3: printTeacher function
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe")); // J. Doe
