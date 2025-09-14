4e72bac6411d6a2c9f8d73fc5ef291710fe30ea4
Truket Lema <truketlema9489@gmail.com>
2025-09-14 19:13:22 +0300

    j

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,5 +1,5 @@
-import { RowID, RowElement } from "./interface";
+import { RowElement } from "./interface";
 
-declare function insertRow(row: RowElement): RowID;
-declare function deleteRow(rowId: RowID): void;
-declare function updateRow(rowId: RowID, row: RowElement): RowID;
+declare function insertRow(row: RowElement): number;
+declare function deleteRow(rowId: number): void;
+declare function updateRow(rowId: number, row: RowElement): number;
